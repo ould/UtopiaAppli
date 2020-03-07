897da67ba534dc91f75a496384023fb25d3e4685
publicitedenis <publicitedenis@outlook.com>
2020-03-07 10:20:20 +0100

    premiere etape

---
+++ TestHebergeurs/TestHebergeurs/Models/Hebergeurs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestHebergeurs.Models
+{
+    public class Hebergeurs
+    {
+
+        public string Nom { get; set; }
+        //public string Prenom { get; set; }
+        public string AdresseEmail { get; set; }
+
+        public List<JoursDeLaSemaine> JoursPreference { get; set; }
+
+
+        public IEnumerable<Disponibilite> Disponibilites { get; set; }
+
+    }
+
+
+
+}
---
+++ TestHebergeurs/TestHebergeurs/Models/LesEnums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestHebergeurs.Models
+{
+    public enum JoursDeLaSemaine
+    {
+        Lundi = 1,
+        Mardi = 2,
+
+
+
+    }
+}
