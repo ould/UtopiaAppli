9630e57a86d8def7a28ce7d54a5312363422fa51
denis <denis@out.com>
2020-03-12 15:48:13 +0100

    gestiondispos

---
+++ TestHebergeurs/TestHebergeurs/Controllers/GestionDispoController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestHebergeurs.Models;
+
+namespace TestHebergeurs.Controllers
+{
+    [Authorize]
+    public class GestionDispoController : Controller
+    {
+        // GET: GestionDispo
+        public ActionResult Index()
+        {
+            var model = new List<Disponibilite>();
+
+            return View(model);
+        }
+
+        // GET: GestionDispo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GestionDispo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GestionDispo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GestionDispo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GestionDispo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GestionDispo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GestionDispo/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- TestHebergeurs/TestHebergeurs/Models/Hebergeurs.cs
+++ TestHebergeurs/TestHebergeurs/Models/Hebergeurs.cs
@@ -9,10 +9,14 @@ namespace TestHebergeurs.Models
     {
 
         public string Nom { get; set; }
-        //public string Prenom { get; set; }
         public string AdresseEmail { get; set; }
+        public bool NLHC { get; set; }
+        public bool Actif { get; set; }
+        public DateTime? DateActivation { get; set; }
+        public string FrequenceHebergement { get; set; }
 
         public List<JoursDeLaSemaine> JoursPreference { get; set; }
+
 
 
         public IEnumerable<Disponibilite> Disponibilites { get; set; }
